985758cb46ad7fa1e71c2ad006e69bd8d76699e2
balagurunathans <balagurunathan.sakthivadivel@gmail.com>
2024-06-18 11:50:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select (col('frui
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 pd_df=st.my_dataframe.to_pandas()
-st.dataframe(pd_df)
+#st.dataframe(pd_df)
 st.stop()
 ingredients_list = st.multiselect ('Choose upto 5 ingredients:', my_dataframe,max_selections=5)
 if ingredients_list:
